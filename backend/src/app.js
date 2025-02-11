1b252317ff7cd7a5128206980ea97b1bbd40bda2
Prachi Shiwakoti <prachishiwakoti@gmail.com>
2025-02-11 21:12:49 +0545

    added role in signup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const connectMongoDB = require('./services/connection');
-const router = require('./routers');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectMongoDB = require("./services/connection");
+const router = require("./routers");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,11 +13,11 @@ app.use(router);
 
 (async () => {
   try {
-    await connectMongoDB(); 
+    await connectMongoDB();
     app.listen(PORT, () => {
       console.log(`Server is running on port http://localhost:${PORT}/`);
     });
   } catch (error) {
-    console.error('Error starting the server:', error);
+    console.error("Error starting the server:", error);
   }
 })();

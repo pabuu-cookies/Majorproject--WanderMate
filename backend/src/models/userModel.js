5f5355b3f6a7345ee3fa29559892b95ef2b6bec6
Pabuu <pkharel156@gmail.com>
2025-02-11 16:38:41 +0545

    added role for th user

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,21 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "guide"],
+    default: "user",
   },
   password: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
